d3514e3b3ad960009f5157c3e0213ef973e27973
Vherniel Lebis <vherniel11@gmail.com>
2025-10-22 08:37:01 +0800

    🔧 config: add alias resolution for module imports during build

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativewind } = require('nativewind/metro');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  '@': path.resolve(__dirname),
+};
 
 module.exports = withNativewind(config);
